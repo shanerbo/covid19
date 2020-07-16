b45ef0c148d3ebafbb9a64a78f33c102478f431f
shanerbo <shanerbo@gmail.com>
2020-07-15 23:30:51 -0700

    make nice and tide smooth curve

--- lib/main.dart
+++ lib/main.dart
@@ -67,11 +67,11 @@ class MyClipper extends CustomClipper<Path> {
   getClip(Size size) {
     print(size);
     var path = Path();
-    path.lineTo(0, size.height-30);
+    path.lineTo(0, size.height - 50);
     path.quadraticBezierTo(
-        size.width / 4, size.height, size.width / 2, size.height - 30);
+        size.width / 4, size.height - 20, size.width / 2, size.height - 50);
     path.quadraticBezierTo(
-        3 * size.width / 4, size.height - 60, size.width, size.height-30);
+        3 * size.width / 4, size.height - 80, size.width, size.height - 50);
     path.lineTo((size.width), 0);
     path.close();
     return path;
